7795d2c9ed11c4257043c9964ff4bb7c6c279d40
Madeline <mross15@uw.edu>
2018-11-28 15:51:32 -0800

    Add app

---
+++ ufoapp/server.R
@@ -0,0 +1,18 @@
+# Load Libraries
+library(shiny)
+library(leaflet)
+
+# Define server logic required to create a map of North America
+shinyServer(function(input, output) {
+    
+    output$mymap <- renderLeaflet({
+      selected_country <- subset(data, Country == toString(input$country))
+      select_shape <- subset(selected_country, Shape == toString(input$shape))
+      leaflet() %>%
+        addTiles() %>%
+        setView(lng = -93.85, lat = 37.45, zoom = 4) %>%
+        addMarkers(data = select_shape, lng = ~lng, lat = ~lat, popup = ~Summary)
+
+    })
+
+})
---
+++ ufoapp/ui.R
@@ -0,0 +1,21 @@
+# Load Libraries
+library(shiny)
+library(leaflet)
+
+# Group the shap column
+shapedata <- data %>% group_by(Shape) %>% summarise()
+
+# Define UI for application that creates a map of North America
+shinyUI(fluidPage(
+  titlePanel("UFO Sightings Data-North America 2016"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("shape", "Select Shape", shapedata), 
+      radioButtons("country", "Select Country",choices = list("USA", "CANADA"),  selected=1)
+    ),
+    mainPanel(
+      leafletOutput("mymap", height = 1000)
+    )
+  )
+  )
+  )
